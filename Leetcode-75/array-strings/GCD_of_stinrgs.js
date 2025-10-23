5b64f8943026f9d3292c0e0c5daf073316c59db1
pratik00020 <pratikkanojia0727@gmai.com>
2025-10-23 15:45:25 +0000

    updated

--- Leetcode-75/array-strings/GCD_of_stinrgs.js
+++ Leetcode-75/array-strings/GCD_of_stinrgs.js
@@ -20,3 +20,41 @@ var gcdOfStrings = function(str1, str2) {
  - Since the compatibility check guarantees both strings can be made by repeating this substring return it as the result.
 
 */
+
+var gcdOfStrings = function(str1,str2) {
+    function gcd(a, b){
+        while(b){
+            [a, b] = [b, a % b];
+        }
+        return a;
+    }
+    if (str1 + str2 !== str2 + str1) return "";
+
+    const lenGCD = gcd(str1.length, str2.length);
+    return str1.slice(0, lenGCD);
+}
+
+var gcdOfStrings = function(str1, str2) {
+    function gcd(a, b){
+       while(b){ [a, b] = [b, a % b];
+       }
+       return a;
+    }
+    if(str1 + str2 !== str2 + str1) return "";
+
+    const lenGCD = gcd(str1.length, str2.length);
+    return str1.slice(0, lenGCD);
+}
+
+var gcdOfStrings = function(str1, str2) {
+    function gcd(a, b){
+        while(b){
+            [a, b] = [b, a % b];
+        }
+        return a;
+    }
+    if (str1 + str2 !== str2 + str1) return "";
+
+    const lenGCD = gcd(str1.length, str2.length);
+    return str1.slice(0, lenGCD);
+}
