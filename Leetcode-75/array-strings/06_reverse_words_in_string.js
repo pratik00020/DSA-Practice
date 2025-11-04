785d3fe28657d718a259367001d4a8256f880673
pratik00020 <pratikkanojia0727@gmail.com>
2025-11-04 22:34:46 +0530

    updated reverse words in string

--- Leetcode-75/array-strings/06_reverse_words_in_string.js
+++ Leetcode-75/array-strings/06_reverse_words_in_string.js
@@ -19,3 +19,23 @@ var reverseWords = function(s) {
     //4. Join the words back into a single string using a single space as a seperator.
     // This ensures only one space is between words and no leading /trailing spaces.
 }
+
+/* ---LOGIC---
+ Preprocess the input string:
+   - apply trim() to remove leading and trailing spaces.
+ word extraction:
+    - apply the split() method to the trimmed string using a regular expression /\s+/ as the delimiter.
+    -\s matches any single whitespace character.
+    - + means "one or more" times.
+    - by splitting on one or more spaces, the function automatically discards all inervening sapces,
+      effectively reducing multiple spaces between words to nothing.
+Reversing the Order:
+    - apply the reverse() method to the array of words.
+    - This method changes the order of the elements in the array in-place, achieving the primary goal of reversing the word sequence.
+Postprocessing and Output:
+    - apply the join(' ') method to the reversed array of words.
+    - This concatenates all the elements in the array back into a single string,
+      inserting the specified argument, a single space(' ') between each word.
+
+
+*/
