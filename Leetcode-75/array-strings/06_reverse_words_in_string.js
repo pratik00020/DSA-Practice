f3a9e378d970be2f808555ca148f5a5abf74e218
pratik00020 <pratikkanojia0727@gmail.com>
2025-11-04 22:22:38 +0530

    updated reverse words in string

--- Leetcode-75/array-strings/06_reverse_words_in_string.js
+++ Leetcode-75/array-strings/06_reverse_words_in_string.js
@@ -6,3 +6,16 @@ const reverseWords = (s) => {
 
     return words.join(' ');
 };
+
+var reverseWords = function(s) {
+    // 1. trim the input string to remove leading and trailing spaces.
+
+    const trimmedString = s.trim();
+    // 2. split the trimmed string into an array of words.
+    // we use the regex /\s+/ (one or more white space characters) to handle cases where there are multiple spaces between words.
+    const words = trimmedString.split(/\s+/);
+    //3. reverse the array of words.
+    words.reverse();
+    //4. Join the words back into a single string using a single space as a seperator.
+    // This ensures only one space is between words and no leading /trailing spaces.
+}
